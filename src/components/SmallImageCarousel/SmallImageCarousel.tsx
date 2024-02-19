05521178b896c3f56a527bfcf17028e0351a0e8f
kareemyasser8 <kareem.yasserr@gmail.com>
2024-02-19 22:48:14 +0200

    fix the image carousel problem

--- src/components/SmallImageCarousel/SmallImageCarousel.tsx
+++ src/components/SmallImageCarousel/SmallImageCarousel.tsx
@@ -11,31 +11,31 @@ import { elementToBeSentOrRemovedFromWishList } from "../../services/wishListSer
 import useIsProductInWishList from "../../Hooks/useIsProductInWishList"
 
 interface Props {
-  product: Product,
+  product: Product
   isInWishList: boolean | undefined
 }
 
-const SmallImageCarousel = ({ product, isInWishList}: Props) => {
-  const [isHeartClicked, setIsHeartClicked] = useState<boolean>(false);
-  const { mutate } = useAddOrDeleteProductToWishList();
-  
-  const prevButtonRef = useRef<HTMLDivElement>(null);
-  const nextButtonRef = useRef<HTMLDivElement>(null);
-  const [scrollPosition, setScrollPosition] = useState<number>(0);
-  
-  useEffect(()=>{
-    if(!isInWishList){
-      setIsHeartClicked(false);
-    }else{
+const SmallImageCarousel = ({ product, isInWishList }: Props) => {
+  const [isHeartClicked, setIsHeartClicked] = useState<boolean>(false)
+  const { mutate } = useAddOrDeleteProductToWishList()
+
+  const imageListRef = useRef<HTMLDivElement>(null)
+  const prevButtonRef = useRef<HTMLDivElement>(null)
+  const nextButtonRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (!isInWishList) {
+      setIsHeartClicked(false)
+    } else {
       setIsHeartClicked(isInWishList)
     }
-  },[isInWishList]);
+  }, [isInWishList])
 
-  const slideScroll = (ref: MutableRefObject<HTMLDivElement | null>, direction: number) => {
-    if (!ref.current) return
-    const scrollAmount = ref.current.clientWidth * direction
-    ref.current.scrollBy({ left: scrollAmount, behavior: "smooth" })
-    setScrollPosition((prevScrollPosition) => prevScrollPosition + scrollAmount)
+  const slideScroll = (ref: MutableRefObject<any>) => {
+    const direction = ref === prevButtonRef ? -1 : 1
+    const scrollAmount =
+      (imageListRef.current?.clientWidth as number) * direction
+    imageListRef.current?.scrollBy({ left: scrollAmount, behavior: "smooth" })
   }
 
   const addOrRemoveProductInWishList = (productId: string) => {
@@ -46,7 +46,6 @@ const SmallImageCarousel = ({ product, isInWishList}: Props) => {
     }
     mutate(elementTobeSent)
   }
-
 
   return (
     <div className="col-12 position-relative">
@@ -69,7 +68,7 @@ const SmallImageCarousel = ({ product, isInWishList}: Props) => {
         className="back-btn"
         style={{ left: "0px", outline: "none", boxShadow: "none" }}
         ref={prevButtonRef}
-        onClick={() => slideScroll(prevButtonRef, -1)}
+        onClick={() => slideScroll(prevButtonRef)}
       >
         <div className="back-btn-circle bg-light mt-5">
           <BsChevronLeft size={"20px"} />
@@ -80,28 +79,33 @@ const SmallImageCarousel = ({ product, isInWishList}: Props) => {
         className="forward-btn"
         style={{ right: "0px" }}
         ref={nextButtonRef}
-        onClick={() => slideScroll(nextButtonRef, 1)}
+        onClick={() => slideScroll(nextButtonRef)}
       >
         <div className="forward-btn-circle bg-light mt-5">
           <BsChevronRight size={"20px"} />
         </div>
       </div>
-
-      <div className="small-carousel-container">
+      <div className="small-carousel-container" ref={imageListRef}>
         <div className="small-carousel-list">
-          {product && product?.Images?.map((img: any, index) => (
-            <div key={index}>
-              <Link to={`/shop/${product.id}`}>
-                <img
-                  className="w-100 object-fit-contain"
-                  style={{ height: "300px" }}
-                  src={img.url}
-                  loading="lazy"
-                  alt={img.preview}
-                />
-              </Link>
-            </div>
-          ))}
+          {product.Images?.map((img: any, index) => {
+            if (index < 2) {
+              return (
+                <div key={index}>
+                  <Link to={`/shop/${product.id}`}>
+                    <img
+                      className="w-100 object-fit-contain"
+                      style={{ height: "300px" }}
+                      // src={APILink + "/" + img.url}
+                      src={img.url}
+                      loading="lazy"
+                      alt={img.preview}
+                    />
+                  </Link>
+                </div>
+              )
+            }
+            return null // Return null for the remaining iterations
+          })}
         </div>
       </div>
     </div>
